95a37d0812f2292c6eb5749874509e56349a0863
Sabetta <t.sabetta@faz.de>
2022-03-30 12:54:54 +0200

    added share sheet on mobile devices

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var wishInput = document.querySelector("#wish span");
 var wishButton = document.querySelector("#wish img");
 var colorSelector = document.querySelector("#colorSelector");
 var hamburgerIcon = document.querySelector("#hamburger");
+var shareIcon = document.querySelector("header img");
 var sidebar = document.querySelector("aside");
 var eyeStatus = "on";
 
@@ -16,36 +17,36 @@ functions
 ---------------------------------------------------------------------------------------------------*/
 function paintEye() {
     event.currentTarget.classList.toggle("visible");
-	localStorage.setItem("Daruma_"+event.currentTarget.id, event.currentTarget.className);
+    localStorage.setItem("Daruma_" + event.currentTarget.id, event.currentTarget.className);
 }
 
 function saveWish() {
-	localStorage.setItem("Daruma_wish", event.currentTarget.textContent);
+    localStorage.setItem("Daruma_wish", event.currentTarget.textContent);
 }
 
 function changeColor() {
-    daruma.src = "svg/daruma-"+ event.currentTarget.value +".svg";
-	localStorage.setItem("Daruma_colorIndex", event.currentTarget.selectedIndex);
-	localStorage.setItem("Daruma_color", event.currentTarget.value);
+    daruma.src = "svg/daruma-" + event.currentTarget.value + ".svg";
+    localStorage.setItem("Daruma_colorIndex", event.currentTarget.selectedIndex);
+    localStorage.setItem("Daruma_color", event.currentTarget.value);
 }
 
 function toggleNav() {
-        sidebar.classList.toggle("open");
-    }
+    sidebar.classList.toggle("open");
+}
 
 function hideText() {
     eyeStatus = (eyeStatus === "on") ? "off" : "on";
     wishInput.classList.toggle("hide");
-    wishButton.src = "svg/eye-"+ eyeStatus +".svg";
+    wishButton.src = "svg/eye-" + eyeStatus + ".svg";
 }
 
 function loadStoredValues() {
-	wishInput.textContent = localStorage.getItem("Daruma_wish") || '';
-	leftEye.className = localStorage.getItem("Daruma_leftEye") || '';
-	rightEye.className = localStorage.getItem("Daruma_rightEye") || '';
-	colorSelector.selectedIndex = localStorage.getItem("Daruma_colorIndex") || 0;
-	var color = localStorage.getItem("Daruma_color") || 'red';
-	daruma.src = "svg/daruma-"+ color +".svg";
+    wishInput.textContent = localStorage.getItem("Daruma_wish") || '';
+    leftEye.className = localStorage.getItem("Daruma_leftEye") || '';
+    rightEye.className = localStorage.getItem("Daruma_rightEye") || '';
+    colorSelector.selectedIndex = localStorage.getItem("Daruma_colorIndex") || 0;
+    var color = localStorage.getItem("Daruma_color") || 'red';
+    daruma.src = "svg/daruma-" + color + ".svg";
     if (localStorage.getItem("Daruma_visited") !== "yes") {
         openSidebar();
     }
@@ -54,6 +55,18 @@ function loadStoredValues() {
 function openSidebar() {
     hamburgerIcon.checked = true;
     sidebar.classList.add("open");
+}
+
+function share() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Dein digitaler Daruma',
+            url: 'https://tehes.github.io/daruma/'
+        })
+    } else {
+        // show share modal
+        console.log("clicked");
+    }
 }
 
 
@@ -68,6 +81,7 @@ function init() {
     wishButton.addEventListener("click", hideText, false);
     colorSelector.addEventListener("change", changeColor, false);
     hamburgerIcon.addEventListener("click", toggleNav, false);
+    shareIcon.addEventListener("click", share, false);
 
     localStorage.setItem("Daruma_visited", "yes");
 }
